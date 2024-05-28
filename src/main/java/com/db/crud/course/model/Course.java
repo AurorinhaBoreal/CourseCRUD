e8bae5bbb0be5cde7db4712e5a9c6b35e4403706
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-05-28 13:25:25 -0300

    feat: Implement Model Student, Teacher, Course and respectively Relationships

---
+++ src/main/java/com/db/crud/course/model/Course.java
@@ -0,0 +1,59 @@
+package com.db.crud.course.model;
+
+import java.util.List;
+
+
+import java.util.ArrayList;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tbl_course")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Course {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_course")
+    private Long Id;
+
+    // Indica o ID do professor que da aula nesse curso, fechando a relação na Entidade Teacher
+    // Muitos Cursos podem ter um (o mesmo) professor
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "course_teacher", nullable = false)
+    private Teacher teacherId;
+
+    @Column(name = "course_name", length = 50)
+    private String name;
+
+    @Column(name = "course_duration", length = 2)
+    private Integer semesters;
+
+    // Vários estudantes, estudam em vários cursos - MANY TO MANY
+    @ManyToMany
+    @JoinTable(
+        name = "tbl_course_students",
+        joinColumns = @JoinColumn(name = "course_id"),
+        inverseJoinColumns = @JoinColumn(name = "student_id")
+    )
+    private List<Student> students = new ArrayList<>();
+}
