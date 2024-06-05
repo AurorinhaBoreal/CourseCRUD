5e945e4f4151ab50249c0921bee37fa9f91d83b3
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-06-05 15:47:26 -0300

    feat: Implements Methods to List, Enroll and Disenroll Students in Course

--- src/main/java/com/db/crud/course/repository/CourseRepository.java
+++ src/main/java/com/db/crud/course/repository/CourseRepository.java
@@ -3,6 +3,9 @@ package com.db.crud.course.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.db.crud.course.model.Course;
@@ -13,4 +16,9 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
 
     Optional<Course> findByCourseId(Long courseId);
     Optional<Course> findByName(String name);
+
+    @Modifying
+    @Query(value = "DELETE FROM tbl_course_students WHERE course_id = :course_id AND student_id = :student_id", nativeQuery = true)
+    void desmatricularAluno(@Param("course_id") Long courseId, @Param("student_id") Long studentId);
+
 }
